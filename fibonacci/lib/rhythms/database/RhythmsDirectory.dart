bb9b9d29681540f926287b834050441a33dfba94
Elias Fazel <elias.fazel@gmail.com>
2024-01-16 10:04:05 -0500

    [ChangeLog] ~
    [Build][ChangeLog]
    ~

--- fibonacci/lib/rhythms/database/RhythmsDirectory.dart
+++ fibonacci/lib/rhythms/database/RhythmsDirectory.dart
@@ -2,9 +2,15 @@
  * Copyright © 2024 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/16/24, 9:58 AM
+ * Last modified 1/16/24, 10:03 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
+String rhythmsCollectionsPath(String emailAddress) {
+
+  String firestorePath = "Fibonacci/Mindset/Profiles/${emailAddress.toUpperCase()}/Rhythms";
+
+  return firestorePath;
+}
