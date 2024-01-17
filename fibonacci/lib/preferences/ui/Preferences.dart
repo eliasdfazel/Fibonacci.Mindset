e8ef80b2316971403a16c0546c3ef59334594364
Elias Fazel <elias.fazel@gmail.com>
2024-01-17 09:28:39 -0500

    [ChangeLog] ~
    [Build][ChangeLog]
    ~

--- fibonacci/lib/preferences/ui/Preferences.dart
+++ fibonacci/lib/preferences/ui/Preferences.dart
@@ -2,14 +2,13 @@
  * Copyright © 2024 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/17/24, 8:47 AM
+ * Last modified 1/17/24, 9:28 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
 import 'package:fibonacci/resources/colors_resources.dart';
-import 'package:fibonacci/resources/strings_resources.dart';
 import 'package:fibonacci/utils/ui/SystemBars.dart';
 import 'package:flutter/material.dart';
 
@@ -34,56 +33,43 @@ class _PreferencesInterfaceState extends State<PreferencesInterface> {
   Widget build(BuildContext context) {
 
     return SafeArea(
-        child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: StringsResources.applicationName(),
-            color: ColorsResources.premiumDark,
-            theme: ThemeData(
-              fontFamily: 'Ubuntu',
-              colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorsResources.premiumDark),
-              pageTransitionsTheme: const PageTransitionsTheme(builders: {
-                TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-                TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-              }),
-            ),
-            home: Scaffold(
-                resizeToAvoidBottomInset: false,
-                backgroundColor: ColorsResources.premiumDark,
-                body: ClipRRect(
-                    borderRadius: BorderRadius.circular(19),
-                    child: Stack(
-                        children: [
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: ColorsResources.premiumDark,
+            body: ClipRRect(
+                borderRadius: BorderRadius.circular(19),
+                child: Stack(
+                    children: [
 
-                          /*
+                      /*
                          * Start - Decoration
                          */
-                          Container(
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage("assets/decoration.png"),
-                                      fit: BoxFit.cover
-                                  )
+                      Container(
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("assets/decoration.png"),
+                                  fit: BoxFit.cover
                               )
-                          ),
+                          )
+                      ),
 
-                          const Align(
-                              alignment: Alignment.centerRight,
-                              child: Opacity(
-                                  opacity: 0.73,
-                                  child: Image(
-                                    image: AssetImage("assets/logo.png"),
-                                    fit: BoxFit.cover,
-                                  )
+                      const Align(
+                          alignment: Alignment.centerRight,
+                          child: Opacity(
+                              opacity: 0.73,
+                              child: Image(
+                                image: AssetImage("assets/logo.png"),
+                                fit: BoxFit.cover,
                               )
-                          ),
-                          /*
+                          )
+                      ),
+                      /*
                          * End - Decoration
                          */
 
 
 
-                        ]
-                    )
+                    ]
                 )
             )
         )
