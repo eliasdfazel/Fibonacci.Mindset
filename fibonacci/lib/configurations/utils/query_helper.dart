508e13befb966d0f0ae2cde0ffd9bf5d5388f039
Elias Fazel <elias.fazel@gmail.com>
2024-01-21 09:02:28 -0500

    [ChangeLog] ~ Adding Configurations Options
    [Build][ChangeLog]
    ~

---
+++ fibonacci/lib/configurations/utils/query_helper.dart
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2024 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 1/21/24, 9:02 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
